21b9edeebad9e4474d758141fde943d365dec36d
jhupo <you@example.com>
2023-12-22 15:33:26 +0800

    init

--- cmake/commondefs.cmake
+++ cmake/commondefs.cmake
@@ -1,9 +1,7 @@
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-
 set(top_dir ${CMAKE_CURRENT_SOURCE_DIR})
-set(top_binary_dir ${CMAKE_CURRENT_BINARY_DIR}/bin)
-set(top_builddir ${CMAKE_CURRENT_BINARY_DIR}/build)
+set(top_binary_dir ${CMAKE_CURRENT_SOURCE_DIR}/bin)
+set(top_builddir ${CMAKE_CURRENT_SOURCE_DIR}/build)
 set(top_include_dir ${CMAKE_CURRENT_SOURCE_DIR}/include)
 set(top_srcdir ${CMAKE_CURRENT_SOURCE_DIR}/src)
 set(top_testdir ${CMAKE_CURRENT_SOURCE_DIR}/test)
@@ -20,3 +18,28 @@ function(search_file dir result)
     )
     SET(${result} ${__FILES_TARGET__} PARENT_SCOPE)
 endfunction()
+
+
+if(NOT cmake_build_type)
+    set(cmake_build_type "Debug")
+endif()
+
+if(cmake_build_type STREQUAL "Debug")
+    set(cmake_build_type "Debug")
+elseif(cmake_build_type STREQUAL "Release")
+    set(cmake_build_type "Release")
+elseif(cmake_build_type STREQUAL "RelWithDebInfo")
+    set(cmake_build_type "RelWithDebInfo")
+else()
+    message(FATAL_ERROR "cmake_build_type must be Debug, Release, RelWithDebInfo or MinSizeRel")
+endif()
+
+add_compile_options(-fPIC)
+
+set(cmake_cxx_flags_debug "$ENV{CXXFLAGS} -O0 -Wall -g -ggdb")
+set(cmake_cxx_flags_release "$ENV{CXXFLAGS} -DNODEBUG -O2 -Wall")
+set(cmake_cxx_flags_relwithdebinfo "$ENV{CXXFLAGS} -Wall -O2 -g -DNDEBUG")
+
+set(cmake_c_flags_debug "$ENV{CFLAGS} -O0 -Wall -g -ggdb")
+set(cmake_c_flags_release "$ENV{CFLAGS} -DNODEBUG -O2 -Wall")
+set(cmake_c_flags_relwithdebinfo "$ENV{CXXFLAGS} -Wall -O2 -g -DNDEBUG")
