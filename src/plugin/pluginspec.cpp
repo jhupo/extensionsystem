700fbaf2d4dbc2d2f6bb215fe018e4e8672b7734
jhupo <you@example.com>
2023-12-28 20:25:02 +0800

    cm:add metadata

--- src/plugin/pluginspec.cpp
+++ src/plugin/pluginspec.cpp
@@ -1,9 +1,22 @@
 #include <plugin/pluginspec.h>
+#include <logger/logger.h>
 
+#include <boost/filesystem.hpp>
 
 namespace extension{
 
     namespace core{
+
+        namespace{
+            const char _plugin_metadata_section[] = "MetaData";
+            const char _plugin_metadata_iid[] = "IID";
+            const char _plugin_metadata_name[] = "Name";
+            const char _plugin_metadata_version[] = "Version";
+            const char _plugin_metadata_description[] = "Description";
+            const char _plugin_metadata_url[] = "Url";
+            const char _plugin_metadata_enabled[] = "Enabled";
+            const char _plugin_metadata_dependency[] = "Dependency";
+        }
 
         class PluginSpecPrivate
         {
@@ -13,6 +26,10 @@ namespace extension{
             PluginSpecPrivate(PluginSpec* q);
             ~PluginSpecPrivate();
 
+            bool read(const std::string& path);
+            bool readMetaData(const std::string& data);
+            
+            std::string read_section_as_string(const std::string& libraryPath, const std::string &sectionName);
 
             PluginSpec*const                q_ptr;
             bool                            _hasError;
@@ -21,13 +38,58 @@ namespace extension{
             std::string                     _version;
             std::string                     _description;
             std::string                     _url;
+            std::string                     _metaData;
+            std::string                     _location;
             bool                            _enabled;
             PluginSpec::State               _state;
             std::vector<PluginDependency>   _dependencys;
             Plugin*                         _plugin;
         };
 
+        PluginSpecPrivate::PluginSpecPrivate(PluginSpec *q)
+            : q_ptr(q)
+        {
 
+        }
+
+        PluginSpecPrivate::~PluginSpecPrivate()
+        {
+            
+        }
+
+        bool PluginSpecPrivate::read(const std::string &path)
+        {
+            _hasError = false;
+            _errorString = _metaData = _pluginName = _version = _description = _url = "";
+            _plugin = DECL_EQ_NULLPTR;
+            _state = PluginSpec::Invalid;
+            _location = boost::filesystem::complete(path).string();
+            if(_location.empty()){
+                _hasError = true;
+                _errorString = "Invalid path";
+                LOG_WRN(extension.core.plugin) << "Invalid path: " << path;
+                return false;
+            }
+            if(!readMetaData(read_section_as_string(_location,".metadata"))){
+                _hasError = true;
+                _errorString = "Invalid metadata";
+                LOG_WRN(extension.core.plugin) << "Invalid metadata: " << _location;
+                return false;
+            }
+            _state = PluginSpec::Read;
+            LOG_INF(extension.core.plugin) << _pluginName <<" Read successful";
+            return true;
+        }
+
+        bool PluginSpecPrivate::readMetaData(const std::string &data)
+        {
+            LOG_DBG(extension.core.plugin) << "Read metadata: " << data;
+            return true;
+        }
+        
+        std::string PluginSpecPrivate::read_section_as_string(const std::string &libraryPath, const std::string &sectionName)
+        {
+            return std::string();
+        }
     }
-
 }
