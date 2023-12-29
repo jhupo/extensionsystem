13950a837c28eabda52a4956849cadbcf725a9c3
jhupo <you@example.com>
2023-12-29 10:47:23 +0800

    cm:modified cmake txt

--- src/plugin/plugin.cpp
+++ src/plugin/plugin.cpp
@@ -74,13 +74,6 @@ namespace extension{
             return d->_spec;
         }
 
-        PLUGIN_METADATA_SECTION
-        static const unsigned char _plugin_metadata[] = 
-        {
-            'P','L','U','G','I','N','_','M','E','T','A','D','A','T','A','_','S','E','C','T','I','O','N','\0'
-        };
-
-        
     }
 
 }
